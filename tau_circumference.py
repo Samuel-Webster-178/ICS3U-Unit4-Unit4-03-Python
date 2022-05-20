7242c34d7f62eada3645b4a9856235aacfd3b17f
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-20 19:13:30 +0000

    fixed

--- tau_circumference.py
+++ tau_circumference.py
@@ -16,9 +16,12 @@ def main():
     # process
     try:
         int_repeat_number = int(str_repeat_number)
-        for counter1 in range(int_repeat_number + 1):
-            product = counter1**2
-            print("{0}² = {1}".format(counter1, product))
+        if int_repeat_number < 0:
+            raise Exception()
+        else:
+            for counter1 in range(int_repeat_number + 1):
+                product = counter1**2
+                print("{0}² = {1}".format(counter1, product))
     except Exception:
         print("Invalid Input")
 
